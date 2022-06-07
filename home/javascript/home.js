f0956be75829f2b4b9c71dcfecc879c6decbaefe
Lakash Dangol <lakash.dangol@gmail.com>
2022-06-07 19:58:24 +0545

    Added auto image changing feature in slider in home page

--- home/javascript/home.js
+++ home/javascript/home.js
@@ -1,3 +1,9 @@
+// For changing Image in the slider
+let carouselControlNextIcon = document.querySelector('.carousel-control-next-icon');
+setInterval(() => {
+    carouselControlNextIcon.click();
+}, 6000);
+
 // Content to be typed in the welcome section
 let welcomeTyped = new Typed('#welcome-typingArea',{
     stringsElement:'#welcome-typingContent',
